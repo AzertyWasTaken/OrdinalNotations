e71066bff7168fa95d3f176ac56a51b8562f9f49
AzertyWasTaken <quentin.guillermin@orange.fr>
2025-09-28 11:26:59 +0200

    Add files via upload

---
+++ main/w^2.js
@@ -0,0 +1,22 @@
+function limit(n) {
+    let a = [];
+    for (let i = 0; i < n; i++) {
+        a.push(1);
+    }
+    return a;
+}
+
+function expand(a, n) {
+    let z = a.pop();
+    if (z == 1) {
+        for (let i = 0; i < n; i++) {
+            a.push(0);
+        }
+    }
+    return a;
+}
+
+console.log(JSON.stringify(limit(3)));
+console.log(JSON.stringify(expand([], 3)));
+console.log(JSON.stringify(expand([0,0,0], 3)));
+console.log(JSON.stringify(expand([1,1], 3)));
---
+++ main/w^3.js
@@ -0,0 +1,27 @@
+function limit(n) {
+    let a = [];
+    for (let i = 0; i < n; i++) {
+        a.push(2);
+    }
+    return a;
+}
+
+function expand(a, n) {
+    let z = a.pop();
+    if (z == 2) {
+        for (let i = 0; i < n; i++) {
+            a.push(1);
+        }
+    } if (z == 1) {
+        for (let i = 0; i < n; i++) {
+            a.push(0);
+        }
+    }
+    return a;
+}
+
+console.log(JSON.stringify(limit(3)));
+console.log(JSON.stringify(expand([], 3)));
+console.log(JSON.stringify(expand([0,0,0], 3)));
+console.log(JSON.stringify(expand([1,1], 3)));
+console.log(JSON.stringify(expand([2,2], 3)));
